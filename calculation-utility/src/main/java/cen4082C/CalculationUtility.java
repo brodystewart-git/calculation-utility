55de73ddac03fc4237528c3e988b665e06bd26c3
Brody Stewart <87514977+brodystewart-git@users.noreply.github.com>
2026-07-13 13:03:03 -0400

    Added logging and safety features

--- calculation-utility/src/main/java/cen4082C/CalculationUtility.java
+++ calculation-utility/src/main/java/cen4082C/CalculationUtility.java
@@ -1,5 +1,8 @@
 package cen4082C;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * This class consists exclusively of mathematical methods that are designed to be used throughout the semester.
  *
@@ -10,22 +13,27 @@ package cen4082C;
  * @author Brody Stewart
  * <p>Course: CEN 4802C - Software Integration, Configuration, and Testing</p>
  * @version 0.1
- * @since 2026-08-26
+ * @since 2026-07-11
  */
 
 public class CalculationUtility {
+	private static final boolean SAFETY_ENABLED = true;
+	private static final Logger logger = LoggerFactory.getLogger(CalculationUtility.class);
 	
 	public static void main(String[] args) {
-		// This section is purely temporary for this specific assignment, so felt no need to make a comment for this main. 
-		// Assuming this will be used as a hub for mathematical functions later in the course, so I doubt main will even really need to exist.
-		int num = 9;
-		System.out.println("HELLO, ALL! MIDTERM TIME!");
-		System.out.println("Current number selected to find in the Fibonacci sequence: " + num);
-		int result = fibonacci(num);
-		if(result == -1) {
-			System.out.println("The number you input was invalid. Please try again with a different number.");
-		}else {
-			System.out.println("The number at position " + num + " of the sequence is " + result);
+		logger.info("Starting Calculation Utility program.");
+		for (int i = -5; i <= 100; i++) {
+			int num = fibonacci(i);
+			
+			if (num >= 0) {
+				logger.info("The number at position {} of the sequence is {}", i, num);
+			} 
+			else if (num == -1) {
+				logger.warn("The number {i} is invalid (negative input). ", i);
+			}
+			if (num == -2) {
+				logger.error("The number as position {} of the sequence is too long and will cause a hang. Calculation halted for safety metrics.", i);
+			}
 		}
 	}
 	
@@ -33,13 +41,14 @@ public class CalculationUtility {
      * Simple Fibonacci sequence that doesn't support negafibonacci. Takes a number n, returning the number in the sequence at that exact location.
      * 
      * <p>This method finds a number in the Fibonacci sequence from the given {@code n} parameter, returning the result.
-     * If the number is negative, it returns a fail state of {@code -1}.</p>
+     * If the number is negative, it returns a fail state of {@code -1}. Includes a safety mechanism to prevent exponential execution lag.</p>
      *
-     * @param n the index in the Fibonacci sequence to calculate (non-negative number)
-     * @return the Fibonacci number at index {@code n}  | -1 if {@code n} is invalid.
+     * @param n the index in the Fibonacci sequence to calculate (non-negative number) 
+     * @return the Fibonacci number at index {@code n}  | -1 if {@code n} is invalid | -2 if safety limits are breached.
      */
 	public static int fibonacci(int n) {
 		if (n < 0) return -1;
+		if(SAFETY_ENABLED && n > 40) return -2;
 		if(n == 0 || n == 1) return n;
 		return fibonacci(n - 1) + fibonacci(n - 2);
 	}
